2af844368d5cb9deff58b9f09a6ac57d6cf39466
ceailo <s46390@student.umt.edu.my>
2023-02-15 00:01:36 +0800

    .

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+/*
+ * Basic Data Types:
+ * - Booleans
+ * - Characters
+ * - Integers
+ * - Floats
+ */
+
+/*
+ * u - unnasigned integers for positive numbers
+ * i - signed integers for negative numbers and positive numbers
+ *
+ * 1 byte = 8 bits
+ *
+ * u8, i8 - 1 byte
+ * u16, i16 - 2 bytes
+ * u32, i32 - 4 bytes
+ * u64, i64 - 8 bytes
+ * u128, i128 - 16 bytes
+ *
+ * architecture dependent size
+ * usize, isize = 4 bytes (x32) / 8 byte (x64)
+ *
+ * f32 - 4 bytes
+ * f64 - 8 bytes
+ * 
+ * bool - 1 bit
+ * 
+ * char - 4 bytes
+ */
+
+ fn main() {
+    println!("Hello, world!");
+}
