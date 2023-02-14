e148fb3d9f18608e11a44f70759b77e8c99b3a6e
ceailo <s46390@student.umt.edu.my>
2023-02-15 00:01:52 +0800

    .

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    /*
+     * No need to infer type for variables name
+     * let mars_weight = calculate_weight_on_mars(100.0);
+     */
+    let mut mars_weight = calculate_weight_on_mars(100.0);
+    // Can't assign to immutable variables
+    mars_weight = mars_weight * 100.0;
+    print!("Weight on Mars: {}g", mars_weight)
+}
+
+/*
+ * Formula for calculating weight on Mars:
+ * weight on Mars = (Weight on Earth / 9.81ms-2) * 3.711ms-2
+ */
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    (weight / 9.81) * 3.711
+}
