05522025eabcb7387637c84c44f5d7dce2f6d74d
ceailo <s46390@student.umt.edu.my>
2023-02-15 00:01:56 +0800

    .

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+/**
+ * Rust bundled with std library by default.
+ * Have collections of:
+ * - Hash maps
+ * - Vectors
+ * - Data Structures
+ * - IO primitives
+ * More info: https://doc.rust-lang.org/std/#modules
+ * IO Modules: https://doc.rust-lang.org/std/io/index.html
+ */
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    // The definition accepts mutable string or mutable string reference
+    io::stdin().read_line(&mut input);
+    let mut mars_weight = calculate_weight_on_mars(100.0);
+    mars_weight = mars_weight * 100.0;
+    print!("Weight on Mars: {}g", mars_weight)
+}
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    (weight / 9.81) * 3.711
+}
