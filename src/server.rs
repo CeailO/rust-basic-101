4789b1d5d049d6b60ca66f88508e7a1a1f0480c2
ceailo <s46390@student.umt.edu.my>
2023-02-16 23:11:27 +0800

    .

---
+++ src/server.rs
@@ -0,0 +1,19 @@
+use std::net::TcpListener;
+
+pub struct Server {
+    addr: String,
+}
+impl Server {
+    pub fn new(addr: String) -> Self {
+        Self { addr }
+    }
+    pub fn run(self) {
+        println!("Listening on {}", self.addr);
+        /*
+         * Pass the reference to the address
+         * Hold the method to read the definition
+         * bind() method will return Result that wrap the TcpListener
+         */
+        let listener = TcpListener::bind(&self.addr);
+    }
+}
