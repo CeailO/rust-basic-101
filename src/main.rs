1cf475cb3c83c58de12d65c983b9fd90b9f1a29f
ceailo <s46390@student.umt.edu.my>
2023-02-16 19:23:00 +0800

    .

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,15 @@
  * Some - expressing the value
  */
 fn main() {
+    let get = Method::GET("abcd".to_string());
+    let delete = Method::DELETE(1);
+    let post = Method::POST;
+    let put = Method::PUT;
+    let patch = Method::PATCH;
+    let head = Method::HEAD;
+    let options = Method::OPTIONS;
+    let trace = Method::TRACE;
+    let connect = Method::CONNECT;
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
@@ -43,4 +52,5 @@ enum Method {
     CONNECT,
     OPTIONS,
     TRACE,
+    PATCH,
 }
