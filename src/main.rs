6e354bbbb1507a5d7446d578dd0ee36a46c15af0
ceailo <s46390@student.umt.edu.my>
2023-02-16 19:49:25 +0800

    ,

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 fn main() {
-    let get = Method::GET("abcd".to_string());
-    let delete = Method::DELETE(1);
-    let post = Method::POST;
-    let put = Method::PUT;
-    let patch = Method::PATCH;
-    let head = Method::HEAD;
-    let options = Method::OPTIONS;
-    let trace = Method::TRACE;
-    let connect = Method::CONNECT;
+    let _get = Method::GET("abcd".to_string());
+    let _delete = Method::DELETE(1);
+    let _post = Method::POST;
+    let _put = Method::PUT;
+    let _patch = Method::PATCH;
+    let _head = Method::HEAD;
+    let _options = Method::OPTIONS;
+    let _trace = Method::TRACE;
+    let _connect = Method::CONNECT;
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
@@ -33,7 +33,7 @@ impl Server {
  * HEADERS \r\n
  * BODY
  */
-struct Request {
+struct _Request {
     method: Method, // must predefine HTTP method
     path: String,
     query_string: String,
